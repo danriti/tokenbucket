403dadf7bece8d545c55a760917c4c18d2d062fe
Dan Riti <dmriti@gmail.com>
2013-12-11 03:25:48 +0000

    Add TokenBucket code.

--- tokenbucket.go
+++ tokenbucket.go
@@ -1,12 +1,29 @@
 package tokenbucket
 
+import (
+	"time"
+)
+
 type TokenBucket struct {
-	foo int
+	rate      int
+	size      int
+	timestamp int64
 }
 
-func New() *TokenBucket {
+func New(rate int, size int) *TokenBucket {
 	tb := &TokenBucket{
-		foo: 1,
+		rate:      rate,
+		size:      size,
+		timestamp: time.Now().Unix(),
 	}
 	return tb
+}
+
+func (tb *TokenBucket) GetTokens() int {
+	now := time.Now().Unix()
+	seconds := int(0)
+	if now > tb.timestamp {
+		seconds = int(now - tb.timestamp)
+	}
+	return seconds * tb.rate
 }
